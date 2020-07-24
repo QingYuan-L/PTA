a6c919afd2faec7ae42b4057893a9f087ade86e8
qingyuan <QingYuan-L@users.noreply.github.com>
2020-07-24 18:04:53 +0800

    自测-5 Shuffling Machine

--- Shuffling Machine/Shuffling_Machine.cpp
+++ Shuffling Machine/Shuffling_Machine.cpp
@@ -5,20 +5,56 @@
 #include <vector>
 using namespace std;
 
+struct poker{
+    char ch;
+    int p_num;
+};
+
+vector<poker> shuffling(vector<poker> pokers,vector<int> order){
+    vector<poker> pokers_temp(54);
+    for(int i=0;i<54;i++)
+    {
+        pokers_temp[order[i]-1].ch=pokers[i].ch;
+        pokers_temp[order[i]-1].p_num=pokers[i].p_num;
+    }
+    return pokers_temp;
+}
+
 int main()
 {
     vector<int> order;
+    vector<poker> pokers_set;
     int order_temp;
+    poker p_temp;
     int num;
     cin >>num;
-    while(cin>>order_temp){
+    char p_f[5]={'S','H','C','D','J'};
+    for(int i=0;i<5;i++)
+        for(int j=1;j<=13;j++)
+        {
+            if(i==4&&j==3)break;
+            p_temp.ch=p_f[i];
+            p_temp.p_num=j;
+            pokers_set.push_back(p_temp);
+        }
+//    for(auto cm:pokers_set)
+//    {
+//        cout<<cm.ch<<cm.p_num<<' ';
+//    }
+//
+    while (cin>>order_temp){
         order.push_back(order_temp);
         char c  = cin.get();
         if (c=='\n')break;
     }
-    for(auto mu:order)
+    for(int i=0;i<num;i++)
     {
-        cout<<mu;
+        pokers_set=shuffling(pokers_set,order);
     }
+    for(int i=0;i<53;i++)
+    {
+        cout<<pokers_set[i].ch<<pokers_set[i].p_num<<' ';
+    }
+    cout<<pokers_set[53].ch<<pokers_set[53].p_num;
     return 0;
 }
